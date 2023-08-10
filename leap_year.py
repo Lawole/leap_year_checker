2407a09a7c536f4ef115386d7631bc42dd976a8e
Dosunmu Olawole <103733962+Lawole@users.noreply.github.com>
2023-08-11 00:21:05 +0100

    Add files via upload

---
+++ leap_year.py
@@ -0,0 +1,16 @@
+
+# LEAP YEAR CHECKER
+
+print("Welcome to Suberu LEAP YEAR CHECKER")
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not leap year")
